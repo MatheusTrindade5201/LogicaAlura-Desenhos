234985f98cec641d88731625ffe2fb94a9fd3784
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-05-16 21:26:45 -0300

    Desenho de esquadro com funções e com API Canvas do JavaScript

---
+++ Esquadro 2/main.js
@@ -0,0 +1,32 @@
+var tela = document.querySelector('canvas');
+var pincel = tela.getContext('2d');
+
+/*pincel.fillStyle = 'black';
+pincel.beginPath();
+pincel.moveTo(50,50);
+pincel.lineTo(50,400);
+pincel.lineTo(400,400);
+pincel.fill();
+pincel.fillStyle = 'white';
+pincel.beginPath();
+pincel.moveTo(100,175);
+pincel.lineTo(100,350);
+pincel.lineTo(275,350);
+pincel.fill();*/
+
+function desenhaTriangulo(xa, ya, xc, yc, cor){
+
+    var tela = document.querySelector('canvas');
+    var pincel = tela.getContext('2d');
+
+    pincel.fillStyle = cor;
+    pincel.beginPath();
+    pincel.moveTo(xa, ya);
+    pincel.lineTo(xa,yc);
+    pincel.lineTo(xc,yc);
+    pincel.fill();
+
+}
+
+desenhaTriangulo(50,50,400,400,'black');
+desenhaTriangulo(100, 175, 275, 350, 'white');
