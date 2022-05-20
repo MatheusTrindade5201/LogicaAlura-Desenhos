cd93b1009125d25dcbdfd01995789a61fd7b7841
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-05-19 21:47:11 -0300

    Animação de movimento com lógica de JavaScript

---
+++ Bolinha/main.js
@@ -0,0 +1,26 @@
+var tela = document.querySelector('canvas');
+var pincel = tela.getContext('2d');
+
+function desenhaBolinha(x, y, raio){
+
+    pincel.fillStyle = 'blue';
+    pincel.beginPath();
+    pincel.arc(x, y, raio, 0, 2 * Math.PI);
+    pincel.fill();
+
+}
+
+function limpaTela() {
+    pincel.clearRect(0,0,600, 400);
+}
+var x = 20;
+var mover = 1;
+var raio = 10;
+function moveBolinha(){
+    limpaTela();
+    desenhaBolinha(x, 20, raio);
+    x += mover;
+    
+}
+
+setInterval(moveBolinha, 5);
