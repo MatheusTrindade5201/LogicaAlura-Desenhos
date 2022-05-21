aff147a855e3b4ba6572508b8b40847c8d790c6e
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-05-20 22:30:00 -0300

    Inclusão de surgimento aleatório do alvo

--- Alvo/main.js
+++ Alvo/main.js
@@ -5,6 +5,8 @@ pincel.fillStyle = 'lightgray';
 pincel.fillRect(0, 0, 600, 400);
 
 var raio = 10;
+var xAleatorio;
+var yAleatorio;
 
 function desenhaCirculo(x, y, raio, cor) {
 
@@ -14,18 +16,37 @@ function desenhaCirculo(x, y, raio, cor) {
     pincel.fill();
 }
 
-desenhaCirculo(300, 200, raio + 20, 'red'); // maior círculo
-desenhaCirculo(300, 200, raio + 10, 'white');
-desenhaCirculo(300, 200, raio, 'red'); // menor circulo
+function desenhaAlvoAleatorio(){
+
+    limpaTela();
+
+    pincel.fillStyle = 'lightgray';
+    pincel.fillRect(0, 0, 600, 400);
+
+    xAleatorio = Math.floor(Math.random()*600);
+    yAleatorio = Math.floor(Math.random()*400);
+
+    desenhaCirculo(xAleatorio, yAleatorio, raio + 20, 'red');
+    desenhaCirculo(xAleatorio, yAleatorio, raio + 10, 'white');
+    desenhaCirculo(xAleatorio, yAleatorio, raio, 'red');
+
+}
+
+setInterval(desenhaAlvoAleatorio, 1000);
+
 
 function dispara(evento) {
 
     var x = evento.pageX - tela.offsetLeft;
     var y = evento.pageY - tela.offsetTop;
 
-    if(x >= 290 && x <= 310 && y >= 190 && y <= 210){
+    if(x >= xAleatorio - raio && x <= xAleatorio + raio && y >= yAleatorio - raio && y <= yAleatorio + raio){
         alert("Você acertou o alvo!");
     }
+}
+
+function limpaTela(){
+    pincel.clearRect(0,0,600,400);
 }
 
 tela.onclick = dispara;
